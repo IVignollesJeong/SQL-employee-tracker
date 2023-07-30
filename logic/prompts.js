7dd574820a6ed320434a1cceb4153fec27a6b07b
Ian Vignolles-Jeong <ianvignolles@gmail.com>
2023-07-30 10:57:10 -0400

    add new role/new department functions

--- logic/prompts.js
+++ logic/prompts.js
@@ -40,22 +40,75 @@ const mainMenu = () => {
     });
 };
 
+
+// EMPLOYEE TABLE ACTIONS (VIEW ALL, ADD, MODIFY)
 const viewAllEmployees = async () => {
     const viewEmployee = await sqlQuery(`SELECT employee.id, 
     employee.first_name, 
     employee.last_name 
     FROM employee`);
     console.log(viewEmployee);
+    mainMenu();
 };
 
+
+// DEPARTMENT TABLE ACTIONS (VIEW ALL, ADD)
 const viewAllDepartments = async () => {
     const dept = await sqlQuery('SELECT * FROM department');
     console.log(dept);
+    mainMenu();
 };
 
+const addNewDepartment = async () => {
+    const newDept = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter the new department name.'
+        }
+    ]);
+    await sqlQuery('INSERT INTO department (name) VALUES (?)', [newDept.name]);
+    console.log(`New department (${newDept.name}) added to database.`);
+    mainMenu();
+}
+
+
+
+// ROLE TABLE ACTIONS (VIEW ALL, ADD)
 const viewAllRoles = async () => {
     const role = await sqlQuery('SELECT * FROM role');
     console.log(role);
+    mainMenu();
 };
+
+const addNewRole = async () => {
+    const allDepts = await sqlQuery('SELECT * FROM department');
+    const deptList = allDepts.map(department => ({
+        name: department.name,
+        value: department.id
+    }));
+
+    const newRole = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Please enter the name of the new role'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Please enter the salary for this new role'
+        },
+        {
+            type: 'list',
+            name: 'department_id',
+            message: 'please select the department this new role is a part of',
+            choices: deptList
+        }
+    ]);
+    await sqlQuery('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [newRole.title, newRole.salary, newRole.department_id]);
+    console.log(`New role ${newRole.title} has been added to department ${newRole.department_id}.`);
+    mainMenu(); 
+}
 
 module.exports = {mainMenu};
