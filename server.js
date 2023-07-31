d884d6bc7b38a69b730f268a1a73bf25ff34b2f0
Ian Vignolles-Jeong <ianvignolles@gmail.com>
2023-07-30 22:41:18 -0400

    removed redundant table package

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // This server.js file will initialize the inquirer prompts in prompts.js. Running this file in the terminal runs the CLI program.
 const inquirer = require('inquirer');
-const { table } = require('table');
 const {mainMenu} = require('./logic/prompts');
 
 mainMenu();
