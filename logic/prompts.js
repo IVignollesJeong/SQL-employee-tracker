b9e032f14a90685d2356b11c882faaeee1469712
Ian Vignolles-Jeong <ianvignolles@gmail.com>
2023-07-30 22:07:19 -0400

    add new employee/ update employee functionality, cleaned code

--- logic/prompts.js
+++ logic/prompts.js
@@ -59,7 +59,93 @@ const viewAllEmployees = async () => {
     mainMenu();
 };
 
+const getEmployees = async () => {
+    const emps = await sqlQuery(`SELECT id, CONCAT(first_name, " ", last_name) AS employee_name FROM employee`);
+    return emps;
+};
 
+const addNewEmployee = async () => {
+    const empRole = await getEmployees();
+    const roleList = empRole.map(role => ({
+        name: role.title,
+        value: role.id
+    }));
+
+    const empManager = await getEmployees();
+    const managerList = empManager.map(manager => ({
+        name: manager.employee_name,
+        value: manager.id
+    }));
+
+    const newEmployee = await inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'Please enter the FIRST name of the new employee:'
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Please enter the LAST name of the new employee:'
+        },
+        {
+            type: 'list',
+            name: 'role_id',
+            message: 'Please select the role assigned to the new employee:',
+            choices: roleList
+        },
+        {
+            type: 'list',
+            name: 'manager_id',
+            message: 'Please select the manager the new employee will report to:',
+            choices: managerList
+        }
+    ]);
+
+    await sqlQuery('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [newEmployee.first_name, newEmployee.last_name, newEmployee.role_id, newEmployee.manager_id]);
+    console.log(`New employee added to database. FIRST: ${newEmployee.first_name} LAST: ${newEmployee.last_name} ROLE: ${newEmployee.role_id} MANAGER: ${newEmployee.manager_id}.`);
+    mainMenu();
+};
+
+
+
+const getRoles = async () => {
+    const allRoles = await sqlQuery('SELECT id, title FROM role');
+    return allRoles;
+};
+
+const updateEmployee = async () => {
+    const emp = await getEmployees();
+    const empList = emp.map(employee => ({
+        name: employee.employee_name,
+        value: employee.id
+    }));
+
+    const rol = await getRoles();
+    const rolList = rol.map(role => ({
+        name: role.title,
+        value: role.id
+    }));
+
+    const employeeUpdated = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employee_id',
+            message: 'Please choose the employee you would like to update:',
+            choices: empList
+        },
+        {
+            type: 'list',
+            name: 'role_id',
+            message: 'Please choose the new role for this employee:',
+            choices: rolList
+        }
+    ]);
+
+    await sqlQuery('UPDATE employee SET role_id = ? WHERE id = ?', [employeeUpdated.role_id, employeeUpdated.employee_id]);
+    console.log('Updated employee role!');
+    mainMenu();
+}
 
 // DEPARTMENT TABLE ACTIONS (VIEW ALL, ADD)
 ////////////////////////////////////////////////////////////////////////////////////////////////
